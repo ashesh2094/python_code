ed738c00054e44db93cdb0807542940f6f241e93
ashesh <patelashesh20194@gmail.com>
2020-04-21 14:15:38 -0500

    find even num of digits

---
+++ hw4.py
@@ -0,0 +1,16 @@
+#  Find Numbers with Even Number of Digits
+
+def findNumbers(nums: List[int]):
+    even = 0
+    for i in nums:
+        count = 0
+        while i:
+            i //= 10
+            count += 1
+        if count % 2 == 0:
+            even += 1
+    return even
+
+
+a= findNumbers([12,345,2,6,7896])
+print(a)
